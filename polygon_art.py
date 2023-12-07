ff8c769fd6130c0b835b58db2e217ebf5d04dfd7
Sariya Punprang <sariya.pu@ku.th>
2023-12-07 11:14:56 +0700

    adding hte last 5-8.jpg and code

--- polygon_art.py
+++ polygon_art.py
@@ -1,54 +1,9 @@
 import turtle
 import random
 
-# def draw_polygon(num_sides, size, orientation, location, color, border_size):
-#     turtle.penup()
-#     turtle.goto(location[0], location[1])
-#     turtle.setheading(orientation)
-#     turtle.color(color)
-#     turtle.pensize(border_size)
-#     turtle.pendown()
-#     for _ in range(num_sides):
-#         turtle.forward(size)
-#         turtle.left(360/num_sides)
-#     turtle.penup()
 
-def get_new_color():
-    return (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-
-
-
-# # draw a polygon at a random location, orientation, color, and border line thickness
-# num_sides = random.randint(3, 5) # triangle, square, or pentagon
-# size = random.randint(50, 150)
-# orientation = random.randint(0, 90)
-# location = [random.randint(-300, 300), random.randint(-200, 200)]
-# color = get_new_color()
-# border_size = random.randint(1, 10)
-# draw_polygon(num_sides, size, orientation, location, color, border_size)
-#
-# # specify a reduction ratio to draw a smaller polygon inside the one above
-# reduction_ratio = 0.618
-#
-# # reposition the turtle and get a new location
-# turtle.penup()
-# turtle.forward(size*(1-reduction_ratio)/2)
-# turtle.left(90)
-# turtle.forward(size*(1-reduction_ratio)/2)
-# turtle.right(90)
-# location[0] = turtle.pos()[0]
-# location[1] = turtle.pos()[1]
-#
-# # adjust the size according to the reduction ratio
-# size *= reduction_ratio
-#
-# # draw the second polygon embedded inside the original
-# draw_polygon(num_sides, size, orientation, location, color, border_size)
-#
-# # hold the window; close it by clicking the window close 'x' mark
-# turtle.done()
 class Art:
-    def __init__(self, choice:int):
+    def __init__(self, choice: int):
         self.choice = choice
         self.random()
         if self.choice == 1:
@@ -103,10 +58,13 @@ class Art:
     def random(self):
         self.orientation = random.randint(0, 90)
         self.location = [random.randint(-300, 300), random.randint(-200, 200)]
-        self.color = get_new_color()
+        self.color = self.get_new_color()
         self.size = random.randint(50, 150)
         self.border_size = random.randint(1, 10)
         self.reduction_ratio = 0.618
+
+    def get_new_color(self):
+        return (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
     def draw_polygon(self):
         turtle.penup()
